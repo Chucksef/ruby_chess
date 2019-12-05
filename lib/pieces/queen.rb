00c1566c3f071bc8d634223cb4aee852693b0ff4
Chucksef <Chucksef@gmail.com>
2019-12-05 16:11:22 -0700

    All movement complete except King

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -10,16 +10,24 @@ class Queen < Piece
         @white_symbol = "♛"
         @black_symbol = "♕"
         @rel_moves = []
+    end
+
+    def get_legal_moves
+        @moves = []
+        directions = [[0,1], [1,0], [0,-1], [-1,0], [1,1], [-1,1], [-1,-1], [1,-1]]
 
         ((1..7).to_a).each do |i|
-            @rel_moves << [i, i] 
-            @rel_moves << [(-1)*i, i]
-            @rel_moves << [i, (-1)*i]
-            @rel_moves << [(-1)*i, (-1)*i]
-            @rel_moves << [0, i] 
-            @rel_moves << [(-1)*i, 0]
-            @rel_moves << [0, (-1)*i]
-            @rel_moves << [(-1)*i, 0]
+            directions.each_with_index do |dir, idx|
+                test_coords = [@position[0] + (dir[0] * i), @position[1] + (dir[1] * i)]
+                next if test_coords[0] > 7 || test_coords[0] < 0 || test_coords[1] > 7 || test_coords[1] < 0
+                piece = @game.get_piece(test_coords)
+                if piece != nil
+                    @moves << test_coords if piece.player != @player 
+                    directions[idx] = [-200,-200]
+                else 
+                    @moves << test_coords
+                end
+            end
         end
     end
 end
