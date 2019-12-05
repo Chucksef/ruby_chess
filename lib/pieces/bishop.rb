f0e8aa1d29301a369aca111ebc9e5c04792a5b5c
Chucksef <Chucksef@gmail.com>
2019-12-05 16:11:22 -0700

    All movement complete except King

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -10,12 +10,24 @@ class Bishop < Piece
         @white_symbol = "♝"
         @black_symbol = "♗"
         @rel_moves = []
+    end
+
+    def get_legal_moves
+        @moves = []
+        directions = [[1,1], [-1,1], [-1,-1], [1,-1]]
 
         ((1..7).to_a).each do |i|
-            @rel_moves << [i, i] 
-            @rel_moves << [(-1)*i, i]
-            @rel_moves << [i, (-1)*i]
-            @rel_moves << [(-1)*i, (-1)*i]
+            directions.each_with_index do |dir, idx|
+                test_coords = [@position[0] + (dir[0] * i), @position[1] + (dir[1] * i)]
+                next if test_coords[0] > 7 || test_coords[0] < 0 || test_coords[1] > 7 || test_coords[1] < 0
+                piece = @game.get_piece(test_coords)
+                if piece != nil
+                    @moves << test_coords if piece.player != @player 
+                    directions[idx] = [-200,-200]
+                else 
+                    @moves << test_coords
+                end
+            end
         end
     end
 end
--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -10,13 +10,6 @@ class Rook < Piece
         @white_symbol = "♜"
         @black_symbol = "♖"
         @rel_moves = []
-
-        ((1..7).to_a).each do |i|
-            @rel_moves << [0, i] 
-            @rel_moves << [(-1)*i, 0]
-            @rel_moves << [0, (-1)*i]
-            @rel_moves << [(-1)*i, 0]
-        end
     end
 
     def get_legal_moves
@@ -32,14 +25,9 @@ class Rook < Piece
                     @moves << test_coords if piece.player != @player 
                     directions[idx] = [-200,-200]
                 else 
-                    puts "adding #{test_coords} to the list"
                     @moves << test_coords
                 end
             end
         end
-        
-        puts
-        puts "legal moves:"
-        @moves.each {|x| puts x.to_s}
     end
 end
